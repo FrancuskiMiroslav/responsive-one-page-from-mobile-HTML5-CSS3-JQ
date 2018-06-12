2cbeed769699cc5224805262bac1073a738dba30
Francuski Miroslav <francuski.miroslav@gmail.com>
2018-06-12 20:42:33 +0200

    first commit adding files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// animate smooth scroll //
+$('#mywork').on('click', function() {
+  const images = $('#images').position().top;
+  
+  $('html, body').animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
