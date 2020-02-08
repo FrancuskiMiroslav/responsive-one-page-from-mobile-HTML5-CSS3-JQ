1ea4ad929b0773057198a25ec323964ffd9a2f65
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-02-08 04:04:35 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 /*! project-name v0.0.1 | (c) 2020 Francuski Miroslav | MIT License | http://link-to-your-git-repo.com */
 // animate smooth scroll //
-$('#mywork').on('click', (function() {
-  const images = $('#images').position().top;
-  
-  $('html, body').animate(
+$("#mywork").on("click", (function() {
+  var images = $("#images").position().top;
+
+  $("html, body").animate(
     {
       scrollTop: images
     },
